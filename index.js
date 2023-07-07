2ac0ec0875e5fe12ace21322f1065976ac9e5acb
Manoj kumar S.P <98573920+Manu05072000@users.noreply.github.com>
2023-07-07 16:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -85,13 +85,4 @@ var swiper = new Swiper(".product-slider", {
       },
     },
   });
-var myVar;
 
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
